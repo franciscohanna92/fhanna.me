b688820679be8b621b8a798c725a625b376f544c
Fracisco Hanna <franciscohanna92@gmail.com>
2024-01-02 12:29:58 -0300

    timeline ui for work experience

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
     <html lang="en" className="bg-stone-100 print:bg-stone-50">
-      <body className={`p-16 print:p-8 prose bg-stone-50 border-x border-stone-200 max-w-4xl mx-auto ${inter.className}`}>
+      <body className={`px-6 py-16 sm:px-16 print:p-8 prose bg-stone-50 border-x border-stone-200 max-w-4xl mx-auto ${inter.className}`}>
         {children}
         {/* <footer>
           <p className="text-sm border-t border-stone-200 pt-8 text-stone-500 mt-10 mb-0">{new Date().getFullYear()}</p>
--- app/page.tsx
+++ app/page.tsx
@@ -24,74 +24,47 @@ const work = [
     title: "Technical Lead",
     subtitle: "Listo · Full Time",
     date: "Mar 2021 - Present",
-    description: `As the Tech Lead at Listo, a communication platform for frontline workers, I’m involved in the process of designing and
-    developing features and solutions from start to finish, leading a team of five developers. I’ve been working in the
-    project since it’s inception and so far I’ve played a critical role in kicking off the platform and scaling it along its
-    userbase.`,
-    highlights: [
-      "Designed and implemented the original MVP for an early adopter.",
-      "Led the engineering effort of the first big refactor, made to take the platform to the next level.",
-      "Migrated the original watch application to Wear OS 3, increasing its availability and robustness.",
-      "Overcame crucial technical challenges to ensure the deliverability of time-critical push notifications.",
-    ],
+    description: `As the Tech Lead at Listo, a communication platform for frontline workers, I've designed and implemented the MVP, led major refactors for scalability, migrated to Wear OS 3, and tackled technical challenges for reliable push notifications. I lead a team of five developers in developing features from inception to delivery.`,
   },
   {
     title: "Sr. Software Engineer",
     subtitle: "SparkDigital · Full Time",
     date: "Jan 2020 - Mar 2021",
-    description: "I worked as a full-stack engineer for two different projects, both for US based companies.",
-    highlights: [
-      "Developed a tech demo application for Verizon Digital Media Services.",
-      "Developed new features for Sapling Learning, an e-learning platform from Macmillan Learning.",
-    ],
+    description:
+      "As a full-stack engineer, I developed a tech demo application for Verizon and new features for Sapling Learning's e-learning platform.",
   },
   {
     title: "Ssr. Software Engineer",
     subtitle: "Plataforma 10 · Full Time",
     date: "Jul 2019 - Dec 2019",
-    description: "Led the newly formed TravelPAQ outsourcing team and was part of one of Plataforma 10’s core back-end teams.",
-    highlights: [
-      "Onboarded and trained new outsourcing team members on Plataforma 10’s tech stack.",
-      "As part of Platforma 10’s Buses team, worked on two new service provider integrations, and generated stress test suites.",
-      "Prepared profiling reports of an API built with GraphQL.",
-      "Carried out maintenance and improvement of core business APIs.",
-    ],
+    description:
+      "I led the TravelPAQ outsourcing team and contributed to Plataforma 10’s core back-end teams, focusing on service provider integrations, stress test suites, and API maintenance.",
   },
   {
     title: "Ssr. Software Engineer",
     subtitle: "MINEXUS · Full Time",
     date: "Oct 2018 - Jun 2019",
     description:
-      "Led a three-person team to build a web-based contractor documentation control system, which consisted of three client web apps, a Single Sign-On service built from scratch and a RESTful API.",
-    highlights: [
-      "Designed the general architecture of the system and the main database schema.",
-      "Developed all the client applications.",
-      "Developed the Single Sign On service following the Password Grant flow from OAuth 2.0.",
-      "Generated the requirements of the RESTful API.",
-    ],
+      "Led a team to build a contractor documentation control system, including client web apps, a Single Sign-On service, and a RESTful API. I was responsible for the architecture, development, and API requirements.",
   },
   {
     title: "Full-Stack Web Developer",
     subtitle: "TravelPAQ · Full Time",
     date: "Jul 2017 - Nov 2018",
-    description: "Developed and improved existing back-office applications for tour package operators and online travel agencies.",
-    highlights: [
-      "Developed a website whitelabel system for travel agencies to offer and sell tour packages online.",
-      "Helped in the migration of the main ETL business process from a monolithic to a microservice-based architecture, exploring serverless solutions.",
-    ],
+    description:
+      "Developed back-office applications and a website whitelabel system for travel agencies. Assisted in migrating ETL processes to a microservice-based architecture.",
   },
   {
     title: "Full-Stack Web Developer",
     subtitle: "HEAL S.A. · Freelance",
     date: "Aug 2017 - Nov 2017",
-    description:
-      "Designed, documented and implemented the an inventory control system, using a real-time web app development framework for a construction company.",
+    description: "Designed, documented, and implemented a real-time web app based inventory control system for a construction company.",
   },
   {
     title: "Web Developer Intern",
     subtitle: "Excelencia Soluciones Informaticas · Internship",
-    date: "Oct 2018 - Jun 2019",
-    description: "Implemented functionalities and provided maintenance of web applications for administration of health care institutions.",
+    date: "Oct 2015 - Apr 2016",
+    description: "Implemented features and provided maintenance for web applications managing health care institutions.",
   },
 ];
 
@@ -159,25 +132,34 @@ export default function Home() {
         </section>
 
         {/* Work experience */}
+
         <section>
           <h2 className="text-xl uppercase font-bold tracking-tight text-stone-800 mt-8 mb-6">Work experience</h2>
 
-          <ul className="list-none pl-0 space-y-8">
-            {work.map((item) => (
-              <li className="pl-0 group relative" key={item.title}>
-                <div className="space-y-1 mb-4">
-                  <h3 className="text-lg font-bold tracking-tight text-stone-800 mt-0 mb-0">{item.title}</h3>
-                  <p className="text-base text-stone-800">{item.subtitle}</p>
-                  <p className="text-base text-stone-800 opacity-50">{item.date}</p>
+          <ul role="list" className="list-none p-0 space-y-8 -ml-1">
+            {work.map((item, index) => (
+              <li key={item.title} className="relative flex gap-x-4 p-0 m-0">
+                {/* line */}
+                {index === work.length - 1 ? null : (
+                  <div className="absolute left-0 top-2 flex w-6 justify-center -bottom-10">
+                    <div className="w-px bg-stone-200"></div>
+                  </div>
+                )}
+                {/* dot */}
+                <div className="relative flex h-6 w-6 flex-none items-center justify-center bg-transparent">
+                  <div className="h-1.5 w-1.5 rounded-full bg-stone-200 ring-1 ring-stone-300"></div>
                 </div>
-                <p className="text-base text-stone-800 mt-2 mb-0">{item.description}</p>
-                <ul className="pl-4">
-                  {item.highlights?.map((highlight) => (
-                    <li className="text-base text-stone-800" key={highlight}>
-                      {highlight}
-                    </li>
-                  ))}
-                </ul>
+                {/* content */}
+                <div>
+                  <div className="space-y-1 mb-4 relative">
+                    <h3 className="text-lg font-bold tracking-tight text-stone-800 mt-0 mb-0">{item.title}</h3>
+                    <p className="text-base text-stone-800">{item.subtitle}</p>
+                    <p className="sm:absolute top-0 right-0 text-base sm:text-sm flex-none py-0.5 ml-auto leading-5 text-gray-500">
+                      {item.date}
+                    </p>
+                  </div>
+                  <p className="text-base text-stone-800 mt-2 mb-0">{item.description}</p>
+                </div>
               </li>
             ))}
           </ul>
